46745f57260b68996740d67fb4ac89505bb0b79c
macmade <macmade@xs-labs.com>
2017-03-24 19:13:39 +0100

    Cleanup...

--- Scripts/travis-after.sh
+++ Scripts/travis-after.sh
@@ -5,6 +5,4 @@ source Scripts/xcenv-lib.sh
 declare -r DIR_BUILD_LIB="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
 source Scripts/xcenv-tests.sh
 declare -r DIR_BUILD_TESTS="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
-source Scripts/xcenv-helper.sh
-declare -r DIR_BUILD_HELPER="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
-xcode-coveralls --include CPPAtomic --project CPPAtomic.xcodeproj "${DIR_BUILD_LIB}" "${DIR_BUILD_TESTS}" "${DIR_BUILD_HELPER}"
+xcode-coveralls --include CPPAtomic --project CPPAtomic.xcodeproj "${DIR_BUILD_LIB}" "${DIR_BUILD_TESTS}"

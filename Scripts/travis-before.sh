9e5167d80571a599ba68b416aeacbea61e89eefa
macmade <macmade@xs-labs.com>
2018-01-28 19:13:13 +0100

    Travis...

--- Scripts/travis-before.sh
+++ Scripts/travis-before.sh
@@ -2,4 +2,4 @@
 
 # Installs xcode-coveralls
 git clone --recursive https://github.com/macmade/xcode-coveralls.git /tmp/xcode-coveralls
-sudo xctool --project /tmp/xcode-coveralls/xcode-coveralls.xcodeproj --scheme xcode-coveralls DSTROOT=/ install
+sudo xcodebuild -project /tmp/xcode-coveralls/xcode-coveralls.xcodeproj -scheme xcode-coveralls DSTROOT=/ install
